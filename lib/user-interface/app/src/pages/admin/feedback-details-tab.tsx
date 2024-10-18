1e25520ae28eebba382c228242f227540e2d9995
Xinrui Ge <xinrui.ge24@gmail.com>
2024-10-18 15:30:22 -0400

    formatting of details page

---
+++ lib/user-interface/app/src/pages/admin/feedback-details-tab.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from '@cloudscape-design/components';
+
+const FeedbackDetailsTab = () => {
+
+    const containerStyle = {
+        backgroundColor: 'white',
+        padding: '16px 20px',
+        borderRadius: '16px',
+        boxShadow: 'var(--shadow-container-mwue46, 0px 0px 1px 1px #e9ebed, 0px 1px 8px 2px rgba(0, 7, 22, 0.12))',
+      };
+
+  return (
+    <div style={containerStyle}>
+        Hello
+    </div>
+  )
+}
+
+export default FeedbackDetailsTab
--- lib/user-interface/app/src/pages/admin/feedback-details.tsx
+++ lib/user-interface/app/src/pages/admin/feedback-details.tsx
@@ -1,27 +1,68 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+import {
+  BreadcrumbGroup,
+  ContentLayout,
+  Header,
+  SpaceBetween,
+  Alert
+} from "@cloudscape-design/components";
+import {
+  Authenticator,
+  Heading,
+  useTheme,
+} from "@aws-amplify/ui-react";
+import BaseAppLayout from "../../components/base-app-layout";
+import useOnFollow from "../../common/hooks/use-on-follow";
+import FeedbackTab from "./feedback-tab";
+import FeedbackPanel from "../../components/feedback-panel";
+import { CHATBOT_NAME } from "../../common/constants";
+import { useState, useEffect } from "react";
+import { Auth } from "aws-amplify";
+import FeedbackDetailsTab from './feedback-details-tab';
 // ChatbotMessage, CreatedAt, FeedbackComments, FeedbackID, Problem, Sources, UserPrompt
 
 const UserFeedbackDetailPage = () => {
 
   const location = useLocation();
   const feedbackItem = location.state?.feedback;
+  const onFollow = useOnFollow();  
 
-  return (
-    <div>
-      <h1>Feedback Details</h1>
-      {feedbackItem ? (
-        <div>
-          <p><strong>Problem:</strong> {feedbackItem.Problem}</p>
-          <p><strong>User Prompt:</strong> {feedbackItem.UserPrompt}</p>
-          <p><strong>Created At:</strong> {feedbackItem.CreatedAt}</p>
-          {/* Render more feedback details here */}
-        </div>
-      ) : (
-        <p>No feedback data available.</p>
-      )}
-    </div>
-  )
+  return (    
+    <BaseAppLayout
+      contentType="cards"
+      breadcrumbs={
+        <BreadcrumbGroup
+          onFollow={onFollow}
+          items={[
+            {
+              text: CHATBOT_NAME,
+              href: "/",
+            },
+
+            {
+              text: "View Feedback",
+              href: "/admin/user-feedback",
+            },
+
+            {
+              text: "Feedback Details",
+              href: `/admin/user-feedback/${feedbackItem.FeedbackID}`,
+            },
+          ]}
+        />
+      }
+
+      content={
+        <ContentLayout header={<Header variant="h1">Feedback Details</Header>}>
+          <SpaceBetween size="l">
+                {/* <FeedbackTab updateSelectedFeedback={setFeedback} selectedFeedback={feedback}/> */}
+                <FeedbackDetailsTab />
+          </SpaceBetween>
+        </ContentLayout>
+      }
+    />
+  );
 }
 
 export default UserFeedbackDetailPage
